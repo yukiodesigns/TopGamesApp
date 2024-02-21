1d20bc3c34c548c10276ba67ce5a80796560a455
Yukio <elsie.atieno@student.moringaschool.com>
2024-02-21 10:53:05 +0300

    Added onclick

--- app/src/main/java/com/example/topgames/GameAdapter.kt
+++ app/src/main/java/com/example/topgames/GameAdapter.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
 class GameAdapter(val gamesList:ArrayList<GameModel>): RecyclerView.Adapter<GameAdapter.MyViewHolder>() {
@@ -17,7 +18,11 @@ class GameAdapter(val gamesList:ArrayList<GameModel>): RecyclerView.Adapter<Game
         init{
            gameImg= itemView.findViewById(R.id.cardView)
            gameTitle = itemView.findViewById(R.id.cardTxt)
-//         itemView.setOnClickListener(){}
+           itemView.setOnClickListener(){
+               Toast.makeText(
+                   itemView.context, "You Chose: ${gamesList[adapterPosition].txtCard}", Toast.LENGTH_SHORT
+               ).show()
+           }
         }
 
     }
